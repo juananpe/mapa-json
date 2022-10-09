a7bc422d352c7aad229087f6bc9f7228da91b5a7
Juanan Pereira <juanan@ikasten.io>
2022-10-09 23:11:51 +0200

    Mapa / JSON

---
+++ mapa.js
@@ -0,0 +1,23 @@
+// mapa sortu ( 14x25 )
+let mapa = new Array(14);
+ for (let i = 0; i < 14; i++) {
+    mapa[i] = new Array(25);
+ }
+
+// mapa hasieratu 
+for(let i=0; i<14; i++){
+    for(let j=0; j<25; j++) {
+        mapa[i][j]=0;
+    }
+}
+
+// mapa bistaratu
+for(let i=0; i<14; i++){
+    let errenkada = []
+    for(let j=0; j<25; j++) {
+        errenkada.push(mapa[i][j])
+    }
+    document.write( errenkada.join(" ") + "<br>" )
+}
+
+
